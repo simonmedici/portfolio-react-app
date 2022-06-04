72f8783bbc35bc69dba1313e1502011c2f77c10a
simonmedici <simon.sebastian.medici@gmail.com>
2022-06-04 10:09:35 -0500

    add api climate-change news project to work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/workImg.jpeg'
 import realEstate from '../assets/realestate.jpg'
+import Worldpng from '../assets/earth_PNG.png'
 
 const Work = () => {
   return (
@@ -14,18 +15,18 @@ const Work = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${Worldpng})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Climate Change Live API
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://rapidapi.com/simonsebastianmedici/api/climate-change-realtime-german/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/simonmedici/climate-change-api/blob/7a7da89f94433215582e4e3ac4095e55d632a7c7/index.js">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
