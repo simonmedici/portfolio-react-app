119e1d39701be602850051ef893a6ee4ad59e3da
simonmedici <simon.sebastian.medici@gmail.com>
2022-06-15 10:15:37 -0500

    change earth api pic

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
-import Worldpng from '../assets/earth_PNG.png';
+import Worldpng from '../assets/earth.jpeg';
 import RealEstatePage from '../assets/realestatepage.jpg';
 import ComingSoon from '../assets/comingsoon.jpg';
 
@@ -15,6 +15,25 @@ const Work = () => {
             </div>
             {/* Container */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/* Grid Item */}
+                <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Real Estate API Website
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://real-estate-web-app-chi.vercel.app/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/simonmedici/real-estate-web-app">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
                 {/* Grid Item */}
                 <div style={{backgroundImage: `url(${Worldpng})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
@@ -34,23 +53,7 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            Real Estate API Website
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="https://real-estate-web-app-chi.vercel.app/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="https://github.com/simonmedici/real-estate-web-app">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
                 {/* Grid Item */}
                 <div style={{backgroundImage: `url(${RealEstatePage})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
