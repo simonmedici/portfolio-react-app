b29b0adfc5eb906d23b8f53b8b8a14086dd1b85f
simonmedici <simon.sebastian.medici@gmail.com>
2022-06-15 09:59:16 -0500

    link of new work project, real estate page. add coming soon placeholders to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import WorkImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
-import Worldpng from '../assets/earth_PNG.png'
+import React from 'react';
+import WorkImg from '../assets/workImg.jpeg';
+import realEstate from '../assets/realestate.jpg';
+import Worldpng from '../assets/earth_PNG.png';
+import RealEstatePage from '../assets/realestatepage.jpg';
+import ComingSoon from '../assets/comingsoon.jpg';
 
 const Work = () => {
   return (
@@ -37,7 +39,7 @@ const Work = () => {
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Real Estate API Website
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="https://real-estate-web-app-chi.vercel.app/">
@@ -50,24 +52,24 @@ const Work = () => {
                     </div>
                 </div>
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${RealEstatePage})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Real Estate Website
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href="/">
+                            <a href="https://real-estate-app-black.vercel.app/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/simonmedici/real-estate-app">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${ComingSoon})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
@@ -85,7 +87,7 @@ const Work = () => {
                     </div>
                 </div>
                 {/* Grid Item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${ComingSoon})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
@@ -102,7 +104,7 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${ComingSoon})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
