3269bb505610770a0adb4173a17db378d7ffeca5
simonmedici <simon.sebastian.medici@gmail.com>
2022-07-13 13:14:47 -0500

    implement getform action again

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[color:var(--background-color)] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/c19d0004-f3b7-4963-a0c0-c95f2dc0d75f" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/99268d76-cd8d-456c-9c2e-19f8379ae39f" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[color:var(--highlight-color)] text-[color:var(--text-color)]'>Contact</p>
                 <p className='text-[color:var(--text-color)] py-4'>Submit the form below or shoot me an email - simon.sebastian.medici@gmail.com</p>
